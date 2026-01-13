177b51873adff6a8bc79ab890e21fbf815c59cfe
MrBeanBoi <129874117+Destruction-Studios@users.noreply.github.com>
2026-01-13 16:50:02 -0500

    changed stuff

---
+++ SpinnyWeb/Managers/VisualDebuggingManager.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace SpinnyWeb
+{
+    public class VisualDebuggingManager
+    {
+        private Vector2 spacing = new Vector2(0, 16);
+        private List<string> textToRender = new();
+        private SpriteFont font;
+        public VisualDebuggingManager(SpriteFont font) 
+        {
+            this.font = font;
+        }
+
+        public void AddText(string text)
+        {
+            textToRender.Add(text);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int i = 0; i < textToRender.Count; i++)
+            {
+                spriteBatch.DrawString(
+                    font,
+                    textToRender[i],
+                    new Vector2(15, 15) +  spacing  * i,
+                    Color.DarkBlue);
+            }
+
+            textToRender.Clear();
+        }
+    }
+}
